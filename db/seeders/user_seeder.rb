a006122ff41d1a5e41067c033670379586098c2b
chadwalt <chadwalt04@gmail.com>
2018-07-18 09:40:29 +0300

    Change double quotes to single quotes

--- db/seeders/user_seeder.rb
+++ db/seeders/user_seeder.rb
@@ -4,19 +4,19 @@ User.destroy_all
 
 users = [
   {
-    first_name: "timothy",
-    last_name: "kyadondo",
-    username: "chadwalt",
-    password: "123",
-    email: "timothy.kyadondo@gmail.com"
+    first_name: 'timothy',
+    last_name: 'kyadondo',
+    username: 'chadwalt',
+    password: '123',
+    email: 'timothy.kyadondo@gmail.com'
   },
   {
-    first_name: "james",
-    last_name: "kasule",
-    username: "james",
-    password: "12345",
-    email: "james.kasule@gmail.com"
-  },
+    first_name: 'james',
+    last_name: 'kasule',
+    username: 'james',
+    password: '12345',
+    email: 'james.kasule@gmail.com'
+  }
 ]
 
 users.each do |user|
